285e71cad319f3e283ef98c6bea97231c9713be4
henzo <84385799+henzopuchetti@users.noreply.github.com>
2022-03-17 09:48:50 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const conexao = async() => {
-    var atlas = await mongoose.connect('mongodb+srv://FaiscaADM:Alexlobo@fiaptecnico.kreph.mongodb.net/ac1tri')
+    var atlas = await mongoose.connect('mongodb+srv://userAdimin:Henzo123@fiaptecnico.jioi3.mongodb.net/ac1tri')
 }
 
 module.exports = conexao
